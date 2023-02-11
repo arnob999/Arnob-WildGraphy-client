df21837839953adcc14f343cd12a6c7062c376cd
Buggy-ARNOB <81729278+Buggy-ARNOB@users.noreply.github.com>
2023-02-11 23:35:54 +0600

    button width solved

--- src/Pages/HomePhoto/HomePhoto.js
+++ src/Pages/HomePhoto/HomePhoto.js
@@ -12,15 +12,22 @@ const HomePhoto = () => {
     }, [])
     console.log(photos)
     return (
-        <div className='flex justify-center flex-col'>
-            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+        <>
+            <div className='flex justify-center flex-col'>
+                <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
 
-                {
-                    photos?.map(photo => <Card key={photo._id} photo={photo}></Card>)
-                }
+                    {
+                        photos?.map(photo => <Card key={photo._id} photo={photo}></Card>)
+                    }
+                </div>
+
             </div>
-            <Link className='btn btn-success glass' to={'myClick'}>See All</Link>
-        </div>
+            <div className='flex justify-center mb-10'>
+
+                <Link className='btn btn-xs sm:btn-sm md:btn-md lg:btn-md' to={'myClick'}>See All</Link>
+            </div>
+        </>
+
     );
 };
 
