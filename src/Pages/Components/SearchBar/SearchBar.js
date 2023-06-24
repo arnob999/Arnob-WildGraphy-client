dffa1cab9670f2eb53789426b6d25d418ebd1f93
arnob999 <arnob196@gmail.com>
2023-06-24 16:55:47 +0600

    setting up searchbar

--- src/Pages/Components/SearchBar/SearchBar.js
+++ src/Pages/Components/SearchBar/SearchBar.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaSearch } from "react-icons/fa"
-
+import './SearchBar.css'
 const SearchBar = () => {
+
+    const [input, setInput] = useState("")
+
+    const fetchData = (value) => {
+        fetch("http://localhost:5000/photos")
+            .then(res => res.json())
+            .then(data => { console.log(data) })
+    }
+
+    const handleChange = (value) => {
+        setInput(value);
+        fetchData(value);
+    }
+
     return (
         <div className='input-wrapper'>
             <FaSearch id='search-icon' />
-            <input type="text" placeholder='Search here' />
+            <input type="text" placeholder='Search here' value={input} onChange={(e) => { handleChange(e.target.value) }} />
         </div>
     );
 };
